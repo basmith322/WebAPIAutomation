13f9b26268d13e29607b581c904088d71df0b161
benjaminsmith <smith-b9@ulster.ac.uk>
2020-05-19 13:16:52 +0100

    Code Refactor

--- src/test/java/Get200.java
+++ src/test/java/Get200.java
@@ -5,6 +5,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -27,31 +28,18 @@ public class Get200 extends BaseClass {
         response.close();
     }
 
-    @Test
-    public void baseUrlReturns200() throws IOException {
-        HttpGet get = new HttpGet(BASE_ENDPOINT);
-
-        response = client.execute(get);
-
-        int actualStatus = response.getStatusLine().getStatusCode();
-
-        assertEquals(actualStatus, 200);
+    @DataProvider
+    private Object[][] endpoints() {
+        return new Object[][] {
+                {""},
+                {"/rate_limit"},
+                {"/search/repositories?q=java"}
+        };
     }
 
-    @Test
-    public void rateLimitReturns200() throws IOException {
-        HttpGet get = new HttpGet(BASE_ENDPOINT + "/rate_limit");
-
-        response = client.execute(get);
-
-        int actualStatus = response.getStatusLine().getStatusCode();
-
-        assertEquals(actualStatus, 200);
-    }
-
-    @Test
-    public void searchReturns200() throws IOException {
-        HttpGet get = new HttpGet(BASE_ENDPOINT + "/search/repositories?q=java");
+    @Test(dataProvider = "endpoints")
+    public void returns200(String endPoints) throws IOException {
+        HttpGet get = new HttpGet(BASE_ENDPOINT + endPoints);
 
         response = client.execute(get);
 
